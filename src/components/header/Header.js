fbef1d72f33167e4ad13671e80c9cdf902b97462
tezrat <successzero7@gmail.com>
2022-10-24 14:21:34 +0100

    Header done

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './Header.css'
-import face from '../../assets/face.jpg'
+import face from '../../assets/profile__img.png'
+import share_img from '../../assets/Icon_1.png'
+import camera_img from '../../assets/Icon.png'
 function Header() {
     return (
-        <div>
-            <img src={face} alt="" className='profile_image' />
-            <p>Annette Black</p>
-            
+        <div className='header'>
+            <div className='profile_image_text'>
+                <div className='image_camera'>
+                    <img src={face} alt="" className='profile_image' />
+                    <img src={camera_img} alt="camera" className='camera' />
+                </div>
+                <p>Annette Black</p>
+            </div>
+            <div className='share'>
+                <img src={share_img} alt="" />
+                <p className='tooltips'>Share link</p>
+            </div>    
         </div>
     )
 }
