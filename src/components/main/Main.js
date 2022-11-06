20efdeca2be25ab286cb2a7e37c1c341bdfdc627
tezrat <successzero7@gmail.com>
2022-11-06 11:37:48 +0100

    the update commit

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 import icongithub from '../../assets/Icon-github.png'
 import slack from '../../assets/slack.png'
 import './Main.css'
-
+import { Link } from 'react-router-dom';
 function Main() {
     return (
         <div className="container">
@@ -12,8 +12,7 @@ function Main() {
             <a href=" https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>" id='book__python'>Python Books</a>
             <a href="https//background.zuri.team" id='pitch'>Background check for coders</a>
             <a href="https://books.zuri.team/design-rules" id='book__design'>Design Books</a>
-            <a href="/contact" id='contact'>Contact Me</a>
-           
+            <Link to="/contact" id='contact'>Contact Me</Link>
             <div className ='container__image'>
                 <img src={slack} alt="slack" className='slack_img'/>
                 <img src={icongithub} alt="github"  className='github_img'/>
